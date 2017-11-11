9d3b9eceba5e919c8eb8c3ab4dbef5dc082f5591
Rafael Thayto <rafa.thayto@gmail.com>
2017-11-11 21:30:32 -0200

    Create components

---
+++ ex.2/component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// Using es6 sintaxe
+export default () => <h1>Primeiro Componente!</h1>;
+
+// Commom sintaxe
+// export default function() {
+//     return <h1>Primeiro Componente!</h1>
+// }
+
---
+++ ex.4/component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// Exportando default ele retorna essa function como padrão
+export default Primeiro = props => (
+    <h1>Primeiro Componente!</h1>
+)
+
+// export const Segundo = props => <h1>Segundo Componente!</h1>
+
+// export { Primeiro, Segundo}
